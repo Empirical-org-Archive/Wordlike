8863ba77796cf5188d53a9eee6663c6ffb10c473
Yorden <jmk1637@rit.edu>
2015-08-19 15:36:50 -0400

    Added comments and cleaned unused code. Changed variable names to be less ambiguous.

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 	$scope.showScore = false;
 	$scope.showGoal = false;
 	$scope.showEndofRoundStatus = false;
-	$scope.showStartButton = true;
 	$scope.showCompletedGoal = false;
 	$http.get("animals.json").success(function(response) { $scope.animals = response.data; });
 
@@ -48,23 +47,22 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 
 	var inactionCounter = 0;
 
-	var totalTimePerWord = 60;
-	$scope.goalNumberWords = 5;
-	$scope.points = 0;
-	$scope.secondsCounter = 0;
+	var totalTimePerWord = 60;//amount of time per round
+	$scope.goalNumberWords = 5;//the number of words to meet the goal
+	$scope.points = 0;//the number of points the player has
+	$scope.secondsCounter = 0;//how many seconds the game has counting for the round, essentially a timer
 
-	$scope.showCompare = false;
-	$scope.lastPointIncrease = 0;
+	$scope.lastPointIncrease = 0;//last point increase the player recieved
 
-	var intervalPromise = null;
-	$scope.active = false;
+	var intervalPromise = null;//the interval has not been set
+	$scope.active = false; //game is set to active
 
-	$scope.wordlist = [];
-	$scope.wordnumber = 0;
-	var listIndex = 0;
-	var synTracker = [];
-	$scope.currentRoundCorrectWords = [];
-	$scope.currentLevelCorrectWords = [];
+	$scope.currentLevelWordList  = []; //the list of word objects for the level
+	$scope.wordnumber = 0;//the word number in the level
+	var listIndex = 0; //the word number in the level
+	var synTracker = []; //the tracker of the synonyms for the current round
+	$scope.currentRoundCorrectWords = [];//list of words the user got correct this round
+	$scope.currentLevelCorrectWords = [];//list of words the user got correct this level
 
 
 	$scope.currentLevelInfo = {};//used to store the all the info about the level retreived from the json file
@@ -89,9 +87,8 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 
 				}
 			}
-			console.log($scope.currentLevelInfo);
 			//gets the json file and sets it to the word list
-			$http.get($scope.currentLevelInfo.levelFile).success(function(response) {$scope.wordlist = response.data; });
+			$http.get($scope.currentLevelInfo.levelFile).success(function(response) {$scope.currentLevelWordList  = response.data; });
 
 
 	}
@@ -99,21 +96,19 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 	$scope.fetchData = function() {
 		//resets the list of words the user got at the end of the round
 		$scope.currentRoundCorrectWords = [];
-
+		//allows the user to input to the text box
 		$scope.setInputState(true);
-
-
-
-		$scope.words = [];
-
-		for (var i = 0; i <= $scope.wordlist[listIndex].synonyms.length - 1; i++) {
+		//array of word objects
+		$scope.currentWordSynonyms = [];
+		//this gives the words the right length
+		for (var i = 0; i <= $scope.currentLevelWordList [listIndex].synonyms.length - 1; i++) {
 
 			var blanks = "";
-			for (var j = 0; j <= $scope.wordlist[listIndex].synonyms[i].syn.length - 1; j++) {
+			for (var j = 0; j <= $scope.currentLevelWordList [listIndex].synonyms[i].syn.length - 1; j++) {
 
 				if(j==0)
 				{
-					blanks += $scope.wordlist[listIndex].synonyms[i].syn.substring(0,1)
+					blanks += $scope.currentLevelWordList [listIndex].synonyms[i].syn.substring(0,1)
 				}
 				else
 				{
@@ -123,18 +118,22 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 			};
 
 			synTracker[i] = i;
-			$scope.words[i] = {word: $scope.wordlist[listIndex].synonyms[i].syn, dummy:blanks, toSwap:1, strike:false, points:""};
+			//currentWordSynonyms is an array of objects that are the synonyms of the current word
+			//word: this is the synonym of the current word
+			//dummy: this is the amount of '.' there are that was given to this word above
+			//toSwap: the current index in the word at which to swap the letters of
+			//strike: whether or not the word has been guessed correctly already
+			//points: the amount of points awarded for getting this word correct
+			$scope.currentWordSynonyms[i] = {word: $scope.currentLevelWordList [listIndex].synonyms[i].syn, dummy:blanks, toSwap:1, strike:false, points:""};
 		}
 
+		//this makes the level playable
 
-		$scope.showCompare = true;
-		$scope.showStartButton = false;
-		$scope.myWord = $scope.wordlist[listIndex].word;
-		$scope.definition = $scope.wordlist[listIndex].definition;
-		$scope.status = "";
-		$scope.wordnumber = listIndex + 1;
-		$scope.lastPointIncrease = 4;
-		startOfRoundCountdownPromise = $interval(countDowntoGameStart, 1000);
+		$scope.currentWord = $scope.currentLevelWordList [listIndex].word;//gets the next word
+		$scope.definition = $scope.currentLevelWordList [listIndex].definition;//gets the words definition
+		$scope.wordnumber = listIndex + 1;//gets the number of the word being used in the level
+		$scope.lastPointIncrease = 0;//sets the initial value for the last score the player got
+		startOfRoundCountdownPromise = $interval(countDowntoGameStart, 1000);//starts the countdown to the gamestart
 
 
 	};
@@ -160,31 +159,33 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 		$scope.input = inputTemp;//sets the new inputTemp to the scopes input
 
 		//this is used to compare the input to each word in the displayed wordlist
-		for (var i = $scope.words.length - 1; i >= 0; i--) {
+		for (var i =  $scope.currentWordSynonyms.length - 1; i >= 0; i--) {
 			//if the words are equal
-			if($scope.words[i].word == $scope.input) {
-				//if the word has not already been entered
-				if(!$scope.words[i].strike){
-					$("#purple-box").hide();
-					$scope.input = "";
-					$scope.words[i].dummy = $scope.words[i].word;
-					$scope.words[i].strike = true;
-					$scope.points += $scope.words[i].word.length - $scope.words[i].toSwap;
-					$scope.words[i].points = "+ " + ($scope.words[i].word.length - $scope.words[i].toSwap);
-					$scope.lastPointIncrease = $scope.words[i].points;
-					$scope.currentRoundCorrectWords.push($scope.words[i]);
-					$scope.currentLevelCorrectWords.push($scope.words[i]);
+			if( $scope.currentWordSynonyms[i].word == $scope.input) {
+				//if the word has not already been entered...
+				if(! $scope.currentWordSynonyms[i].strike){
+					$("#purple-box").hide();//hides the purple box for the score
+					$scope.input = "";//resets the input box
+					$scope.currentWordSynonyms[i].dummy =  $scope.currentWordSynonyms[i].word; // sets the dummy the full word
+					$scope.currentWordSynonyms[i].strike = true; //the word is complete, puts a strike through it
+					$scope.points +=  $scope.currentWordSynonyms[i].word.length -  $scope.currentWordSynonyms[i].toSwap; // adds the points from this word to the players points
+					$scope.currentWordSynonyms[i].points = "+ " + ( $scope.currentWordSynonyms[i].word.length -  $scope.currentWordSynonyms[i].toSwap);
+					$scope.lastPointIncrease = $scope.currentWordSynonyms[i].points;//sets tehg last point increase to the score just recieved
+					$scope.currentRoundCorrectWords.push($scope.currentWordSynonyms[i]);//adds the current word to the words correct from this round
+					$scope.currentLevelCorrectWords.push($scope.currentWordSynonyms[i]);//adds the current word to the words correct from this level
 
-					userRef.update({points: $scope.points});
-					fadeOutBox(2000);
+					userRef.update({points: $scope.points});//updates the users amount of points
 
-					trackerRemove(i);
+					fadeOutBox(2000);//fades out the box from the
+
+					trackerRemove(i);//removes the word from the tracker
 					return;
 				}
 				else {
 					return;
 				}
 			}
+			//clears the input if enter is pressed
 			if(pressedEnter == true){
 					$scope.input = "";
 
@@ -247,10 +248,10 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 		$scope.showEndofRoundStatus = false;
 		$scope.wordnumber = 0;
 		$scope.secondsCounter = totalTimePerWord;
-		$scope.wordlist = [];
-		$scope.words = [];
+		$scope.currentLevelWordList  = [];
+		$scope.currentWordSynonyms = [];
 		$scope.status = '';
-		$scope.myWord = '';
+		$scope.currentWord = '';
 		$scope.definition = '';
 		$scope.lastPointIncrease = 0;
 		listIndex = 0;
@@ -265,7 +266,7 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 		$scope.switchState(4);
 
 	}
-
+	//this formats the time so that it appears as "1:00"
 	$scope.formatTime = function(){
 		var time = "";
 		var minutes = Math.floor($scope.secondsCounter / 60)
@@ -278,7 +279,7 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 		return time;
 
 	}
-
+	//this is used to fade out the purple box that shows the last score the player got
 	var fadeOutBox = function(time){
 		$("#purple-box").stop(true, true);
 		$("#purple-box").show();
@@ -286,7 +287,7 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 			$("#purple-box").fadeOut("fast");
 		}, time);
 	}
-
+	//this is used to countsdown to when the player has
 	var countDowntoGameStart = function(){
 		$scope.lastPointIncrease --;
 		$("#purple-box").show();
@@ -324,6 +325,7 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 
 
 	//gets the player list and sorts them by the amount of points they have
+	//also sorts out players that appear as null or undefined
 	var sortPlayers = function(){
 
 		$scope.sortedPlayerList = [];
@@ -374,14 +376,14 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 	//checks to see if there are any words left for the player to fill in
 	var checkWordsLeft = function(){
 		var wordcount = 0;
-		for(var i=0; i < $scope.words.length; i++)
+		for(var i=0; i < $scope.currentWordSynonyms.length; i++)
 		{
-			if($scope.words[i].strike == true)
+			if($scope.currentWordSynonyms[i].strike == true)
 			{
 				wordcount++;
 			}
 		}
-		if(wordcount == $scope.words.length)
+		if(wordcount == $scope.currentWordSynonyms.length)
 		{
 			return true;
 		}
@@ -389,7 +391,8 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 			return false;
 		}
 	}
-
+	//this contains the logic for when the user runs out of time in the level
+	//it sorts the players list, and shows whether that player met their goal of correct words.
 	var endRound = function(){
 		sortPlayers();
 		if($scope.currentRoundCorrectWords.length >= $scope.goalNumberWords){
@@ -404,38 +407,35 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 		$scope.switchState(3);
 	}
 
-
-	//Timer functions and stuff
+	//this is fired every second on the game
+	//it gets a random word from the list of synonyms and swaps a letter out for it.
 	var inactionTrigger = function() {
 
 		inactionCounter = 0;
 
-		var wordIndex = Math.round(Math.random() * (synTracker.length -1));
-		var item = $scope.words[synTracker[wordIndex]];
-		item.dummy = replaceAt(item.dummy, item.toSwap, item.word.charAt(item.toSwap));
-		item.toSwap++;
-		//console.log(item.toSwap);
-
+		var wordIndex = Math.round(Math.random() * (synTracker.length -1));//gets a random word index
+		var item = $scope.currentWordSynonyms[synTracker[wordIndex]];//gets the word from the above index
+		item.dummy = replaceAt(item.dummy, item.toSwap, item.word.charAt(item.toSwap));//replaces the dummy with the new dummy
+		item.toSwap++;//increses that synonym objects toSwap value
+		//removes the word from the tracker if it is fully revealed
 		if(item.toSwap == item.word.length - 1) {
 
 			trackerRemove(synTracker[wordIndex]);
 		}
 	}
-
+	//when the player runs out of time
 	var outOfTime = function() {
-
-		$scope.showScoreButton = true;
-		$scope.status = "Out of time. Revealing answers.";
+		//increases to the next word
 		listIndex++;
 		inactionCounter = 0;
+		//reveals all the words
+		for (var i = $scope.currentWordSynonyms.length - 1; i >= 0; i--) {
 
-		for (var i = $scope.words.length - 1; i >= 0; i--) {
-
-			$scope.words[i].dummy = $scope.words[i].word;
-			$scope.words[i].toSwap = $scope.words[i].word.length - 1;
+			$scope.currentWordSynonyms[i].dummy = $scope.currentWordSynonyms[i].word;
+			$scope.currentWordSynonyms[i].toSwap = $scope.currentWordSynonyms[i].word.length - 1;
 		}
-
-		if(listIndex == $scope.wordlist.length) {
+		//checks to see if there is a next level
+		if(listIndex == $scope.currentLevelWordList .length) {
 
 			if(checkIfNextLevel(levelList) == true)
 			{
@@ -449,19 +449,15 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 
 			return;
 		}
-
-
-
-
 	}
-
+	//starts the time and creates the intervalPromise object
 	var timerStart = function() {
 		$scope.secondsCounter = totalTimePerWord;
 		$scope.active = true;
 
 		if (intervalPromise == null) {intervalPromise = $interval(timerTick, 1000); }
 	}
-
+	//stops the timer
 	var timerStop = function() {
 
 		if($interval.cancel(intervalPromise)){
@@ -471,12 +467,14 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 		intervalPromise = null;
 		$scope.active = false;
 	}
-
+	//this is called everytime the the timer tickds (every one second)
 	var timerTick = function() {
-	$("#main-input").focus();
-
+	$("#main-input").focus();//focus on the textbox
+		//if the game is active
 		if($scope.active) {
+			//reduce time by one
 			$scope.secondsCounter--;
+			//add to the inaction counter
 			inactionCounter++;
 
 			//checks if all the words have been entered
@@ -484,13 +482,11 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 			{
 				$scope.secondsCounter = 0;
 			}
-			if($scope.secondsCounter == 0) {
-
+			//if the player has run out of time
+			if($scope.secondsCounter == 0)
+			{
 	        	timerStop();
 	        	outOfTime();
-
-
-
 			}
 			else if(inactionCounter == 1) {
 				inactionTrigger();
@@ -505,11 +501,11 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
 
 	}
 
-
+	//sets the promise to the timer tick when the game starts
 	intervalPromise = $interval(timerTick, 1000);
 
 
-	//Connetivity and Multiplayer stuff
+	//connects to Firebase and checks whether the user is online
 	amOnline.on('value', function(snapshot) {
    		if (snapshot.val()) {
      		userRef.onDisconnect().remove();
@@ -517,7 +513,9 @@ function ($scope, $http, $timeout ,$sce, $window, $location, $compile, $interval
   		}
 	});
 }]);
+//below are the directives thatare used in the index.html
 
+//this directive is used to put the game state in the tag labeled appdir, it uses the synonym-template.html page as its template
 app.directive('appdir', function ($templateCache) {
 
 	return {
@@ -527,6 +525,7 @@ app.directive('appdir', function ($templateCache) {
 	};
 });
 
+//this directive is used to put the header in the tag labeled head, it uses the header-template.html page as its template
 app.directive('headtemp', function ($templateCache) {
 
 	return {
@@ -535,6 +534,7 @@ app.directive('headtemp', function ($templateCache) {
 	};
 });
 
+//this directive is used to put the goal page in the tag labeled goaltem, it uses the goal-template.html page as its template
 app.directive('goaltemp', function ($templateCache) {
 
 	return {
@@ -543,6 +543,7 @@ app.directive('goaltemp', function ($templateCache) {
 	};
 });
 
+//this directive is used to put the level list in the tag labeled levellist, it uses the levellist-template.html page as its template
 app.directive('levellist', function ($templateCache) {
 
 	return {
@@ -551,6 +552,7 @@ app.directive('levellist', function ($templateCache) {
 	};
 });
 
+//this directive is used to put the score template in the tag labeled scoretemp, it uses the score-template.html page as its template
 app.directive('scoretemp', function ($templateCache) {
 
 	return {
